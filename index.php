a9907669c554d24da41f7c6a7ada8f7423cfcb8b
bfxmarques <felipe06consentino@gmail.com>
2024-04-26 13:46:59 -0300

    site mercado 24hrs

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php?>
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <title>Mercado 24hr</title>
+</head>
+
+<body>
+   
+    <div class="container_nav">
+        <img src="style/files/logo.png" alt=""> 
+    <form class="container_search" action="Search_Page.php" method="get"> 
+        <input placeholder="Utilize para pesquisar produtos ex: alface, leite, salgadinho" type="text"> 
+        <button type="submit"><i class="fas fa-search"></i></button> <!-- Magnifying Search Button Icon --> 
+    </form>     
+             <button class="cart_btn" type="button"><i class="fas fa-shopping-cart"></i><!-- Shop Cart Button Icon-->
+    </div>
+
+<div class="container_navitem">
+    <a href="">alimentos</a>
+    <a href="">eletronicos</a>
+    <a href="">eletrodomesticos</a>
+    <a href="">moveis</a>
+</div>
+
+</body>
+
+</html>  
