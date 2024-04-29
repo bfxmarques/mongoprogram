a03792939eeb76113c49fcc9cbc284a887936a9d
bfxmarques <felipe06consentino@gmail.com>
2024-04-29 17:53:09 -0300

    mercado 24hr

---
+++ components/category_menu.php
@@ -0,0 +1,8 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $('.category_button').click(function(){
+                $('.category_menu').toggle();
+            });
+        });
+    </script>
--- index.php
+++ index.php
@@ -1,32 +1,120 @@
-<?php?>
+
 <!DOCTYPE html>
 <html lang="pt">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <title>Mercado 24hr</title>
-</head>
-
-<body>
+    <link rel="stylesheet" href="./style/indexstyle.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-icShn41n5smxGVW4/Is/O+Eih0Ug1PtWp/7KlQVqpGZwZ6hBEXIh9PGSRPq2l3NNkzFFXtLIUd86He91rrbV8Q==" crossorigin="anonymous" />
+    <title>mercado 24hr</title>
    
-    <div class="container_nav">
-        <img src="style/files/logo.png" alt=""> 
-    <form class="container_search" action="Search_Page.php" method="get"> 
-        <input placeholder="Utilize para pesquisar produtos ex: alface, leite, salgadinho" type="text"> 
-        <button type="submit"><i class="fas fa-search"></i></button> <!-- Magnifying Search Button Icon --> 
-    </form>     
-             <button class="cart_btn" type="button"><i class="fas fa-shopping-cart"></i><!-- Shop Cart Button Icon-->
+</head>
+<body>
+    <header>
+        <div class="container-fluid">
+            <div class="rectangle bg-primary"></div>
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <img class="logo" src="style/files/newlogo.png" alt="logo site"> 
+                    </div>
+                    <div class="col">
+                        <h1>Mercado 24 Horas</h1>
+                    </div>
+                    <div class="col">
+                        <nav>
+                            <ul>
+                                <li><a href="#">Página Inicial</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="col">
+                        <div class="search-container" id="searchContainer">
+                          <img src="style/files/lupa.png" alt="Lupa" id="searchIcon">
+                            <input type="text" id="searchInput" placeholder="Pesquisar...">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="content">
+        <!-- Conteúdo -->
     </div>
 
-<div class="container_navitem">
-    <a href="">alimentos</a>
-    <a href="">eletronicos</a>
-    <a href="">eletrodomesticos</a>
-    <a href="">moveis</a>
+    <script>
+        document.getElementById('searchIcon').addEventListener('mouseenter', function() {
+            document.getElementById('searchInput').classList.add('expanded');
+        });
+
+        document.getElementById('searchContainer').addEventListener('mouseleave', function() {
+            document.getElementById('searchInput').classList.remove('expanded');
+        });
+    </script>
+</body>
+</html>
+
+
+
+
+
+<!-- aqui para baixo e conteudo -->
+
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="style/files/example1.jpg" class="d-block w-100" alt="img1">
+    </div>
+    <div class="carousel-item">
+      <img src="style/files/example2.jpg" class="d-block w-100" alt="img2">
+    </div>
+    <div class="carousel-item">
+      <img src="style/files/example3.jpg" class="d-block w-100" alt="img3">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
 
-</body>
+    <div class="container_produtos">
+        <div class="produtos">
+            
+        <div class="prod">
+        <img src="style/files/example1.jpg" alt="ovos">
+        <p>kjashdjklah</p>
+        </div>
 
-</html>  
+        <div class="prod">
+        <img src="style/files/example1.jpg" alt="sucrilhos">
+        <p>aujsghdjkladsa</p>
+        </div>
+
+        <div class="prod">
+        <img src="style/files/example1.jpg" alt="nescau">
+        <p>ajghsdjkahsd</p>
+        </div>
+
+        <div class="prod">
+        <img src="style/files/example1.jpg" alt="katchup">
+        <p>kayhsdjkahsd</p>
+        </div>
+        
+        </div>
+    </div>
+
+
+</body>
+</html>
